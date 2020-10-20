8e851ef1b65269cbab65a9de11c33c6a6347154c
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-10-20 16:04:45 +0100

    Update studentManagement.py

--- studentManagement.py
+++ studentManagement.py
@@ -1,11 +1,12 @@
 from statistics import mean
+import matplotlib.pyplot as plt
 class student:
     def __init__(self, name, age, iden, lMark):
         self.name=name
         self.age=age
         self.iden=iden
         self.lMark=lMark
-    def printAve(self):
+    def calcAve(self):
         return mean(self.lMark)
     def disInfo(self, obj):
         print("the name is: ",obj.name)
@@ -23,6 +24,22 @@ class student:
     def removeStudent(self, _val):
         i = self.searchStudent(_val)
         del ls[i]
+    def showResult(self):
+        for i in range (len(ls)):
+            lis=[]
+            for j in range(1,len(self.lMark)+1):
+                lis.append(j)
+            plt.scatter(lis,ls[i].lMark, label=ls[i].name)
+            plt.legend()
+            #plt.plot(self.calcAve())
+            #plt.axis("tight")
+            #plt.ylabel(ls[i].name)
+            #plt.show()
+    def passOrFail(self, obj):
+        if obj.calcAve()>=10:
+            print("The student "+obj.name+" passes")
+        else:
+            print("The student "+obj.name+" fails")
 
 ls=[]
 s=student("Paul", 23,14, [15,16,17,20,12])
@@ -31,10 +48,14 @@ added = s.addStudent("Van", 20, 12, [12,18,14,16])
 if added:
     print("added success")
 print("the student num: ",s.searchStudent(23)+1, " was found")
+s.showResult()
+plt.show()
 for i in range (len(ls)):
-    print(ls[i].printAve())
+    print(ls[i].calcAve())
 for i in range(len(ls)):
     s.disInfo(ls[i])
+for i in range (len(ls)):
+    s.passOrFail(ls[i])
 print("after deletion: ")
 s.removeStudent(20)
 for i in range (len(ls)):
