1af2ee7e7ee1c8aa19eb9730dba2558655936a03
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-12-01 19:43:14 +0100

    Delete music_genre_prediction.py

--- music genre predection/music_genre_prediction.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier as dtc
-
-my_data=pd.read_csv('music.csv')
-input=my_data.drop(columns=['genre'])
-output=my_data['genre']
-print(output)
